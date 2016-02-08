e887245ca311b466df14afa60103841d826aa579
Brendan <ros0021@jmss.vic.edu.au>
2016-02-08 15:13:56 +1100

    A small file used to test exceptions with "try".

--- Testing.py
+++ Testing.py
@@ -1,10 +1,9 @@
-bubba = True
-while bubba:
-    try:
-        x = input("Type a big juicy whole number.\n")
-        x = int(x)
-        bubba = False
-        print ("Yeah good job you did it nice congratulations good effort nice job thats pretty cool man bomb diggity you typed a", x)
-    except ValueError:
-        print ("What in the hell is '", x, "' are you stupid try again")
-print ("A WOWIE")
+x = input("Type a big juicy whole number.\n")
+
+#if x=int(x):
+ #   print ("Yeah good job you did it nice congratulations good effort nice job thats pretty cool man bomb diggity you typed a", x)
+#elif x=float(x):
+#    print ("I said a whole number alright?")
+#else:
+ #       print ("What in the hell is '", x, "' are you stupid try again")
+print (x = int(x))
