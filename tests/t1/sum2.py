658deccf6d818d45cb302f1496a511c20b1dc21a
Brendan <ros0021@jmss.vic.edu.au>
2016-02-29 15:11:14 +1100

    These are the tests we were given in class. I completed 2 out of 3 of them.

--- tests/t1/sum2.py
+++ tests/t1/sum2.py
@@ -1,2 +1,18 @@
 # copy the code from sum1.py into this file, THEN:
 # change your program so it keeps reading numbers until it gets a -1, then prints the sum of all numbers read
+global history
+history = 0
+def jj(m):
+    global history
+    a = input("Input a number.")
+    if int(a) == -1:
+        return
+    else:
+        history = int(a) + int(history)
+        jj(m)
+m = 1
+jj(m)
+print(history)
+
+# I don't know how to use loops to be honest;
+# I've been using functions like so when loops are required.
--- tests/t1/words.py
+++ tests/t1/words.py
@@ -1,2 +1,19 @@
 # Write a program to read in words from the keyboard one at a time until the word "quit" is typed.
 # Store them in a list then print them alphabetically
+global history
+history = []
+def jj(m):
+    global history
+    a = input("Input a word.")
+    if a == "quit":
+        return
+    else:
+        a = str(a)
+        history.append(a)
+        jj(m)
+m = 1
+jj(m)
+print(history)
+
+# I know this code prints the list however I do not know how to sort the list alphabetically.
+# I explored using things like history.sort(x) however I do not know how things like that
